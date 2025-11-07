630254946bb9422dd8c646c99a525defc1d8cf11
nattloopez <158845483+nattloopez@users.noreply.github.com>
2025-11-07 10:23:11 +0100

    Update compile.sh to read main2

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ mkdir -p compiled
 cat > build.m <<END
 addpath(genpath('brainstorm3/deploy'))
 addpath(genpath('jsonlab'))
-mcc -m -R -nodisplay -d compiled main
+mcc -m -R -nodisplay -d compiled main2
 exit
 END
 matlab -nodisplay -nosplash -r build
